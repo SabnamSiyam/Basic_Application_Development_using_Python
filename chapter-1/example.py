64119c1fd1ff6f7da5a2b49644c143ac1d2140ce
Sbnam Ahmed Siyam <sabnamahmedsiyam263#gmail.com>
2023-04-27 19:14:29 +0600

    first commit

---
+++ chapter-1/example.py
@@ -0,0 +1,9 @@
+def Area():
+    lenght = int(input('Enter the value of lenght:'))
+    width = int(input('Enter the value of width:'))
+    RectangleArea = lenght*width
+    print('Area of Rectangle is=', RectangleArea)
+
+
+
+Area()
